ddbbcf9b066ec1e96b0787e99c6f1e405d4847a9
Naglis Kazlauskas <Naglis.Kazlauskas@ibm.com>
2021-12-08 21:16:25 +0000

    Feat(App): fixing player apollo bug

--- components/Player.js
+++ components/Player.js
@@ -1,34 +1,36 @@
 import { useSession } from "next-auth/react";
 import useSpotify from "../hooks/useSpotify";
-import { GET_SONG } from "../graphql/reactivities/songReactivites";
+import { GET_SONG, isPlaying } from "../graphql/reactivities/songReactivites";
 import { useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
 import useSongInfo from "../hooks/useSongInfo";
+import { SwitchHorizontalIcon } from "@heroicons/react/outline";
 
 function Player() {
 	const spotifyAPI = useSpotify();
 	const { data } = useQuery(GET_SONG);
+	console.log(data);
 	const { currentTrackIdState: currentTrack, isPlaying: playing } = data;
 	const { data: session } = useSession();
 	const songInfo = useSongInfo();
+	const [volume, setVolume] = useState(50);
 
 	useEffect(() => {
 		async function fetchCurrentsong() {
 			if (!songInfo) {
-				console.log("Now playing: ", data.body?.item);
 				const data = await spotifyAPI.getMyCurrentPlayingTrack();
-				spotifyAPI.setCurrentIdTrack(data.body?.item?.id);
-
+				console.log("Now playing: ", data?.body?.item);
+				if (data?.body?.item) currentTrackIdState(data.body.item.id);
 				const currentPlaying = await spotifyAPI.getMyCurrentPlaybackState();
-				spotifyAPI.setIsPlaying(currentPlaying.body?.is_playing);
+				if (currentPlaying.body?.is_playing === true) isPlaying(true);
 			}
 		}
 		if (spotifyAPI.getAccessToken() && !currentTrack) {
-			fetchCurrentsong(setVolume(50));
+			fetchCurrentsong();
+			setVolume(50);
 		}
 	}, [currentTrack, spotifyAPI, session, songInfo]);
 
-	const [volume, setVolume] = useState(50);
 	return (
 		<div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
 			{/* left */}
@@ -44,6 +46,8 @@ function Player() {
 					<p>{songInfo?.artists?.[0]?.name}</p>
 				</div>
 			</div>
+			{/* center */}
+			<SwitchHorizontalIcon className="button" />
 		</div>
 	);
 }

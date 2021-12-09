af0b896c534d05d7bc76a607aaed4df963145f01
Naglis Kazlauskas <Naglis.Kazlauskas@ibm.com>
2021-12-09 22:16:58 +0000

    Feat(App): Adding volume controls

--- components/Player.js
+++ components/Player.js
@@ -6,7 +6,7 @@ import {
 	currentTrackIdState,
 } from "../graphql/reactivities/songReactivites";
 import { useQuery } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import useSongInfo from "../hooks/useSongInfo";
 import {
 	SwitchHorizontalIcon,
@@ -15,7 +15,10 @@ import {
 	PauseIcon,
 	PlayIcon,
 	FastForwardIcon,
+	VolumeUpIcon,
+	VolumeOffIcon,
 } from "@heroicons/react/solid";
+import { debounce } from "lodash";
 
 function Player() {
 	const spotifyAPI = useSpotify();
@@ -57,6 +60,23 @@ function Player() {
 		}
 	}, [currentTrack, spotifyAPI, session, songInfo]);
 
+	useEffect(() => {
+		if (volume > 0 && volume < 100) {
+			debounceAdjustVolume(volume);
+		}
+	}, [debounceAdjustVolume, volume]);
+
+	const debounceAdjustVolume = useCallback(
+		debounce((volume) => {
+			try {
+				spotifyAPI.setVolume(volume);
+			} catch (err) {
+				console.error(err);
+			}
+		}, 500),
+		[]
+	);
+
 	return (
 		<div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
 			{/* left */}
@@ -89,6 +109,25 @@ function Player() {
 					onClick={() => spotifyAPI.skipToNext()}
 				/>
 				<ReplyIcon className="button" />
+			</div>
+			{/* right side */}
+			<div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+				<VolumeOffIcon
+					onClick={() => volume > 0 && setVolume(volume - 10)}
+					className="button"
+				/>
+				<input
+					type="range"
+					min={0}
+					max={100}
+					value={volume}
+					className="w-14 md:w-28"
+					onChange={(e) => setVolume(Number(e.target.value))}
+				/>
+				<VolumeUpIcon
+					className="button"
+					onClick={() => volume < 100 && setVolume(volume + 10)}
+				/>
 			</div>
 		</div>
 	);

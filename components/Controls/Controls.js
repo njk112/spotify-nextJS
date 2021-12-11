b116ee2336a8a18d69c611f8d99599fc8b2eda8c
Naglis Kazlauskas <Naglis.Kazlauskas@ibm.com>
2021-12-11 16:38:51 +0000

    pretty

--- components/Controls/Controls.js
+++ components/Controls/Controls.js
@@ -4,27 +4,23 @@ import {
 	RewindIcon,
 	PauseIcon,
 	PlayIcon,
-	FastForwardIcon,
+	FastForwardIcon
 } from "@heroicons/react/solid";
 import { useQuery } from "@apollo/client";
-import {
-	GET_SONG,
-	isPlaying,
-	changedSong
-} from "@songReactivity";
+import { GET_SONG, isPlaying, changedSong } from "@songReactivity";
 import useSpotify from "@hooks/useSpotify";
 
-function Controls () {
-    const { data } = useQuery(GET_SONG);
-    const spotifyAPI = useSpotify();
+function Controls() {
+	const { data } = useQuery(GET_SONG);
+	const spotifyAPI = useSpotify();
 
-    const {
+	const {
 		currentTrackIdState: currentTrack,
 		isPlaying: playing,
 		changedSong: songHasChanged
 	} = data;
 
-    const handlePlayPause = async () => {
+	const handlePlayPause = async () => {
 		try {
 			const currentPlaying = await spotifyAPI.getMyCurrentPlaybackState();
 			if (currentPlaying.body?.is_playing) {
@@ -57,25 +53,25 @@ function Controls () {
 		}
 	};
 
-    return (
-        <div className="flex items-center justify-evenly">
-        <SwitchHorizontalIcon className="button" />
-        <RewindIcon className="button" onClick={handlePrevious} />
-        {playing ? (
-            <PauseIcon
-                className="button w-10 h-10"
-                onClick={handlePlayPause}
-            />
-        ) : (
-            <PlayIcon
-                className="button w-10 h-10"
-                onClick={handlePlayPause}
-            />
-        )}
-        <FastForwardIcon className="button" onClick={handleNext} />
-        <ReplyIcon className="button" />
-    </div>
-    )
+	return (
+		<div className="flex items-center justify-evenly">
+			<SwitchHorizontalIcon className="button" />
+			<RewindIcon className="button" onClick={handlePrevious} />
+			{playing ? (
+				<PauseIcon
+					className="button w-10 h-10"
+					onClick={handlePlayPause}
+				/>
+			) : (
+				<PlayIcon
+					className="button w-10 h-10"
+					onClick={handlePlayPause}
+				/>
+			)}
+			<FastForwardIcon className="button" onClick={handleNext} />
+			<ReplyIcon className="button" />
+		</div>
+	);
 }
 
 export default Controls;
